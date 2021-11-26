f2516b8d2dbd40dec3f1b12fb6d066b0bf23762a
tuguldur <tuguldur@tuguldurs-MacBook-Pro.local>
2021-11-27 01:08:44 +0800

    exception added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 const AWS = require("aws-sdk");
-
 exports.handler = async (event, context) => {
   let result = null;
+  let browser = null;
+
+  try {
+  } catch (error) {
+    console.log(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
   return result;
 };
